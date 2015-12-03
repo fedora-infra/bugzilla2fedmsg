1863f1959915a522686b1341029da56d3bedf30c
Ralph Bean <rbean@redhat.com>
2015-12-03 14:08:32 -0500

    Got moved again.

--- bugzilla2fedmsg.py
+++ bugzilla2fedmsg.py
@@ -84,8 +84,8 @@ def convert_datetimes(obj):
 
 class BugzillaConsumer(moksha.hub.api.Consumer):
 
-    # This is /topic/com.redhat.bugzilla in STOMP land.
-    topic = '/topic/com.redhat.bugzilla'
+    # This is the fedora_from_esb A-MQ queue.
+    topic = '/queue/fedora_from_esb'
 
     def __init__(self, hub):
         super(BugzillaConsumer, self).__init__(hub)
